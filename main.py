8b16c13e536fba7cd4513db74d48544d5fa6658c
Lynx <Lynxat0mic02@gmail.com>
2019-10-10 21:08:29 +0100

    experimenting with subprocess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import passInfo as nextPass
+from time import sleep
+import subprocess
 
 
 def make_time(time):
@@ -10,11 +12,17 @@ next_pass = nextPass.get_pass_info(print_info=False)
 #
 print(next_pass.name, next_pass.freq, make_time(next_pass.aos), make_time(next_pass.max_e), make_time(next_pass.los))
 
+
 def record():
     # do something here
-    print("done record")
+    bashCommand = "rtl_fm -M wbfm -f 89.1M | play -r 32k -t raw -e s -b 16 -c 1 -V1 - &"
+    subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+
+
 
 def serial_az_el():
     # sends data over serial to arduino format (az, el)
-    print("done sent data")
+    for i in range(0, 10):
+        sleep(1)
+        print("f2")
 
